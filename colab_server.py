2fafc80571a3428b50e7e7c6abc7204aafba66ab
Thịnh Nguyễn <ntnthinh205@gmail.com>
2026-05-10 21:02:03 +0700

    fix(intent_pb2_grpc): syntax issues

--- colab_server.py
+++ colab_server.py
@@ -5,17 +5,18 @@ import threading
 import time
 
 # Update these paths based on where you cloned the repo in Colab
-PROJECT_PATH = '/content/finetune-banking77' # Adjust this to your repo name
-sys.path.append(f"{PROJECT_PATH}/scripts")
+# PROJECT_PATH = '/content/finetune-banking77' # Adjust this to your repo name
+# sys.path.append(f"{PROJECT_PATH}/scripts")
 
 from protos import intent_pb2
 from protos import intent_pb2_grpc
 from scripts.inference import IntentClassification
 
+MODEL_PATH = "/content/drive/MyDrive/Năm 3/Kỳ 2/Ứng dụng xử lý ngôn ngữ tự nhiên trong doanh nghiệp /Bài tập/BT02: Finetune (unsloth)/checkpoints"
 class IntentServer(intent_pb2_grpc.IntentServiceServicer):
     def __init__(self):
         print("Loading Model...")
-        self.model = IntentClassification(model_path=f"{PROJECT_PATH}/checkpoints")
+        self.model = IntentClassification(model_path=MODEL_PATH)
         print("Model Ready!")
 
     def PredictIntent(self, request, context):
